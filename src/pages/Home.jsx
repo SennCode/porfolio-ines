284622463977a1031dad2c9ea6b10c809b84bc27
SennCode <andres.alonso.senn@gmail.com>
2023-08-09 13:03:49 +0200

    primeras rutas6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,50 +2,45 @@ import { useState } from "react";
 import "../App.css";
 
 const Home = () => {
-  const [hoveredText, setHoveredText] = useState(null);
+  const texts = ["Hello", "I am", "Inés"];
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
   const handlePageClick = () => {
-    if (hoveredText === "Hello") {
-      setHoveredText("About");
-    
-    }
+    setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
   };
 
   return (
-    <div className="bg-white bg-[url(./assets/images/IMG_9EB17B5FFAD7-1.jpeg)] bg-no-repeat bg-right min-h-screen bg-auto md:bg-contain flex items-center" onClick={handlePageClick}>
+    <div
+      className="bg-white bg-[url(./assets/images/IMG_9EB17B5FFAD7-1.jpeg)] bg-no-repeat bg-right min-h-screen bg-auto md:bg-contain flex items-center"
+      onClick={handlePageClick}
+    >
       <div className="mx-6 justify-start py-10">
         <div className="px-8">
           <h1
-            className={`text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl  
-             transition-transform transform-gpu ${
-               hoveredText === "Hello" ? "translate-x-8" : null
+            className={`text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu ${
+               texts[currentTextIndex] === "Hello" ? "translate-x-8" : null
              }`}
-            onMouseEnter={() => setHoveredText("Hello")}
-            onMouseLeave={() => setHoveredText(null)}
           >
-            {hoveredText === "Hello" ? "About" : "Hello."}
+            {texts[currentTextIndex] === "Hello" ? "Hello." : "About"}
           </h1>
 
           <h1
             className={`text ${
-              hoveredText === "I am" ? "translate-x-8" : null
+              texts[currentTextIndex] === "I am" ? "translate-x-8" : null
             } font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
              transition-transform transform-gpu`}
-            onMouseEnter={() => setHoveredText("I am")}
-            onMouseLeave={() => setHoveredText(null)}
           >
-            {hoveredText === "I am" ? "Proyects" : "I am"}
+            {texts[currentTextIndex] === "I am" ? "Work" : "I am"}
           </h1>
 
           <h1
             className={`text ${
-              hoveredText === "Inés" ? "translate-x-8" : null
+              texts[currentTextIndex] === "Inés" ? "translate-x-8" : null
             } font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl
              transition-transform transform-gpu`}
-            onMouseEnter={() => setHoveredText("Inés")}
-            onMouseLeave={() => setHoveredText(null)}
           >
-            {hoveredText === "Inés" ? "Contact" : "Inés"}
+            {texts[currentTextIndex] === "Inés" ? "Contact" : "Inés"}
           </h1>
         </div>
       </div>
