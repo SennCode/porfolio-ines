36a6ef7848c65c807c594a602e4273b65dfbeff8
SennCode <andres.alonso.senn@gmail.com>
2023-08-09 17:50:56 +0200

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,47 @@ const Home = () => {
   const [textIam, setTextIam] = useState("I am");
   const [textInes, setTextInes] = useState("Inés");
 
+  // onMouseEnter
+
   const handleMouseEnterHello = () => {
     if (!isMobile) {
       setTextHello(textHello === "Hello." ? "About" : "Hello.");
     }
   };
 
+  const handleMouseEnterIam = () => {
+    if (!isMobile) {
+      setTextIam(textIam === "I am" ? "Work" : "I am");
+    }
+  };
+
+  const handleMouseEnterInes = () => {
+    if(!isMobile){
+      setTextInes(textInes === "Inés" ? "Contact" : "Inés")
+    }
+  }
+
+  // onMouseLeave
+
   const handleMouseLeaveHello = () => {
     if (!isMobile) {
       setTextHello("Hello.");
     }
   };
+
+  const handleMouseLeaveIam = () => {
+    if (!isMobile) {
+      setTextIam("I am");
+    }
+  };
+
+  const handleMOuseLeaveInes = () => {
+    if(!isMobile) {
+      setTextInes("Inés")
+    }
+  }
+
+  // handleClickText
 
   const handleClickText = () => {
     if (isMobile) {
@@ -34,25 +64,30 @@ const Home = () => {
     >
       <div className="mx-6 justify-start py-10">
         <div className="px-8">
-          <h1
-            className="text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-            transition-transform transform-gpu"
-            onMouseEnter={() => handleMouseEnterHello()}
-            onMouseLeave={handleMouseLeaveHello}
-          >
-            {textHello}
-          </h1>
+        <h1
+  className={`text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+    transition-transform transform-gpu ${textHello === "Hello." && !isMobile ? "-translate-x-8" : ""}`}
+  onMouseEnter={handleMouseEnterHello}
+  onMouseLeave={handleMouseLeaveHello}
+>
+  {textHello}
+</h1>
+
 
           <h1
-            className="font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-            transition-transform transform-gpu "
+            className={`font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu ${textIam === "I am" && !isMobile ? "-translate-x-8" : ""}`}
+            onMouseEnter={handleMouseEnterIam}
+            onMouseLeave={handleMouseLeaveIam}
           >
             {textIam}
           </h1>
 
           <h1
-            className="text font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl
-             transition-transform transform-gpu"
+            className={`font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu ${textInes === "Inés" && !isMobile ? "-translate-x-8" : ""}`}
+             onMouseEnter={handleMouseEnterInes}
+             onMouseLeave={handleMOuseLeaveInes}
           >
             {textInes}
           </h1>
