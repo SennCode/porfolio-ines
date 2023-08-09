f3a7e76308871cb8e94ef674a19c40cb86025643
SennCode <andres.alonso.senn@gmail.com>
2023-08-09 16:01:58 +0200

    primeras rutas7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,60 @@
 import { useState } from "react";
 import "../App.css";
+import { isMobile } from "react-device-detect";
 
 const Home = () => {
-  const texts = ["Hello", "I am", "Inés"];
-  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [textHello, setTextHello] = useState("Hello.");
+  const [textIam, setTextIam] = useState("I am");
+  const [textInes, setTextInes] = useState("Inés");
 
-  const handlePageClick = () => {
-    setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+  const handleMouseEnterHello = () => {
+    if (!isMobile) {
+      setTextHello(textHello === "Hello." ? "About" : "Hello.");
+    }
+  };
+
+  const handleMouseLeaveHello = () => {
+    if (!isMobile) {
+      setTextHello("Hello.");
+    }
+  };
+
+  const handleClickText = () => {
+    if (isMobile) {
+      setTextHello(textHello === "Hello." ? "About" : "Hello.");
+      setTextIam(textIam === "I am" ? "Work" : "I am");
+      setTextInes(textInes === "Inés" ? "Contact" : "Inés");
+    }
   };
 
   return (
     <div
       className="bg-white bg-[url(./assets/images/IMG_9EB17B5FFAD7-1.jpeg)] bg-no-repeat bg-right min-h-screen bg-auto md:bg-contain flex items-center"
-      onClick={handlePageClick}
+      onClick={handleClickText}
     >
       <div className="mx-6 justify-start py-10">
         <div className="px-8">
           <h1
-            className={`text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-            transition-transform transform-gpu ${
-               texts[currentTextIndex] === "Hello" ? "translate-x-8" : null
-             }`}
+            className="text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu"
+            onMouseEnter={() => handleMouseEnterHello()}
+            onMouseLeave={handleMouseLeaveHello}
           >
-            {texts[currentTextIndex] === "Hello" ? "Hello." : "About"}
+            {textHello}
           </h1>
 
           <h1
-            className={`text ${
-              texts[currentTextIndex] === "I am" ? "translate-x-8" : null
-            } font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-             transition-transform transform-gpu`}
+            className="font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu "
           >
-            {texts[currentTextIndex] === "I am" ? "Work" : "I am"}
+            {textIam}
           </h1>
 
           <h1
-            className={`text ${
-              texts[currentTextIndex] === "Inés" ? "translate-x-8" : null
-            } font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl
-             transition-transform transform-gpu`}
+            className="text font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl
+             transition-transform transform-gpu"
           >
-            {texts[currentTextIndex] === "Inés" ? "Contact" : "Inés"}
+            {textInes}
           </h1>
         </div>
       </div>
