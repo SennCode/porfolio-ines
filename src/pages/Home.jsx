4fdc01558bf197fbe21cc35eeec7ace2e4b57a31
SennCode <andres.alonso.senn@gmail.com>
2023-08-10 10:24:28 +0200

    home terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,10 +22,10 @@ const Home = () => {
   };
 
   const handleMouseEnterInes = () => {
-    if(!isMobile){
-      setTextInes(textInes === "Inés" ? "Contact" : "Inés")
+    if (!isMobile) {
+      setTextInes(textInes === "Inés" ? "Contact" : "Inés");
     }
-  }
+  };
 
   // onMouseLeave
 
@@ -42,10 +42,10 @@ const Home = () => {
   };
 
   const handleMOuseLeaveInes = () => {
-    if(!isMobile) {
-      setTextInes("Inés")
+    if (!isMobile) {
+      setTextInes("Inés");
     }
-  }
+  };
 
   // handleClickText
 
@@ -59,35 +59,38 @@ const Home = () => {
 
   return (
     <div
-      className="bg-white bg-[url(./assets/images/IMG_9EB17B5FFAD7-1.jpeg)] bg-no-repeat bg-right min-h-screen bg-auto md:bg-contain flex items-center"
+      className="bg-white bg-[url(./assets/images/prueba-bg2.jpeg)] bg-no-repeat bg-right min-h-screen bg-auto md:bg-cover flex justify-start items-center"
       onClick={handleClickText}
     >
-      <div className="mx-6 justify-start py-10">
-        <div className="px-8">
-        <h1
-  className={`text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-    transition-transform transform-gpu ${textHello === "Hello." && !isMobile ? "-translate-x-8" : ""}`}
-  onMouseEnter={handleMouseEnterHello}
-  onMouseLeave={handleMouseLeaveHello}
->
-  {textHello}
-</h1>
-
-
+      <div className="mx-20 py-10">
+        <div className="transition ease-in-out delay-950 hover:translate-x-10 hover:duration-1500 px-8">
           <h1
-            className={`font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-            transition-transform transform-gpu ${textIam === "I am" && !isMobile ? "-translate-x-8" : ""}`}
+            className="animate-fade-up animate-once animate-duration-1500 animate-delay-10000 animate-ease-in text-red-colour font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+    transition-transform transform-gpu "
+            onMouseEnter={handleMouseEnterHello}
+            onMouseLeave={handleMouseLeaveHello}
+          >
+            {textHello}
+          </h1>
+        </div>
+
+        <div className="transition ease-in-out delay-950 hover:translate-x-10 hover:duration-1500 px-8">
+          <h1
+            className="animate-fade-up animate-once animate-duration-25000 animate-delay-500 animate-ease-in font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu"
             onMouseEnter={handleMouseEnterIam}
             onMouseLeave={handleMouseLeaveIam}
           >
             {textIam}
           </h1>
+        </div>
 
+        <div className="transition ease-in-out delay-950 hover:translate-x-10 hover:duration-1500 px-8">
           <h1
-            className={`font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
-            transition-transform transform-gpu ${textInes === "Inés" && !isMobile ? "-translate-x-8" : ""}`}
-             onMouseEnter={handleMouseEnterInes}
-             onMouseLeave={handleMOuseLeaveInes}
+            className="animate-fade-up animate-once animate-duration-40000 animate-delay-1000 animate-ease-in font-bold text-7xl sm:text-7xl md:text-9xl lg:text-9xl xl:text-9xl 
+            transition-transform transform-gpu"
+            onMouseEnter={handleMouseEnterInes}
+            onMouseLeave={handleMOuseLeaveInes}
           >
             {textInes}
           </h1>
